8833362bb2050e2d31583507f62a951ec3f0d6db
lighthouseee <buihaidagggg@gmail.com>
2024-11-25 11:05:33 +0700

    Thay doi ten file

--- Menu.py
+++ Menu.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-from crud.CRUD import read_csv_data, create_data, update_data, delete_data, paginate_data
+from crud.crud import read_csv_data, create_data, update_data, delete_data, paginate_data
 from tool.tool import sort_data, search_data, filter_data
 import pandas as pd
 
